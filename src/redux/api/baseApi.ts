13d038c2b6e693008df7b264f7c623eaa6a7266a
rabby <rabby.65301@gmail.com>
2024-09-02 18:23:01 +0600

    add backend api link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,20 +5,21 @@ import { logOut, setUser } from '../features/auth/authSlice';
 import { toast } from 'sonner';
 
 
+
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api',
-    credentials:'include',
+    baseUrl: `${import.meta.env.VITE_SERVER_API_LINK}/api`,
+    credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
 
         const token = (getState() as RootState).auth.token;
-           
-            
-            if (token) {
-                headers.set("Authorization", `Bearer ${token}`) 
-                
-            }
-           
-       
+
+
+        if (token) {
+            headers.set("Authorization", `Bearer ${token}`)
+
+        }
+
+
         return headers;
 
 
@@ -29,21 +30,21 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs, BaseQueryApi, DefinitionType> = async (arg, api, extraOptions): Promise<any> => {
 
     let result = await baseQuery(arg, api, extraOptions);
-    
+
     // console.log('base api inside ==>', result)
 
-    if(result.error?.status ===404){
+    if (result.error?.status === 404) {
         toast.error(result.error.data.message)
-      
-        
+
+
     }
 
-    if(result.error?.status ===403){
+    if (result.error?.status === 403) {
         toast.error(result.error.data.message)
     }
     if (result.error?.status === 401) {
         // send Refresh Token
-        const res = await fetch('http://localhost:3000/api/auth/refresh-token', {
+        const res = await fetch(`${import.meta.env.VITE_SERVER_API_LINK}/api/auth/refresh-token`, {
             credentials: "include",
             method: "POST",
         });
@@ -76,7 +77,7 @@ const baseQueryWithRefreshToken: BaseQueryFn<FetchArgs, BaseQueryApi, Definition
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    tagTypes:['facility','bookings'],
+    tagTypes: ['facility', 'bookings'],
     baseQuery: baseQueryWithRefreshToken,
     endpoints: () => ({}),
 
